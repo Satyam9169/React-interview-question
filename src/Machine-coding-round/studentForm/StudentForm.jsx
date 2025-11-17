d14adad3a237c774ce6d5a7b6151abe417c96523
satyam agrahari <satyamagrahari952@gmail.com>
2025-11-17 10:49:28 +0530

    code splitting for student form

--- src/Machine-coding-round/studentForm/StudentForm.jsx
+++ src/Machine-coding-round/studentForm/StudentForm.jsx
@@ -1,7 +1,76 @@
 import React from "react";
 
 const StudentForm = ({ form, error, handleChange, handleSubmit }) => {
-  return <div>StudentForm</div>;
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="first_name">First Name :</label>
+        <input
+          type="text"
+          id="first_name"
+          name="first_name"
+          value={form.first_name}
+          onChange={handleChange}
+        />
+        <span style={{ color: red }}>{error.first_name}</span>
+        <br />
+        <label htmlFor="last_name">Last Name :</label>
+        <input
+          type="text"
+          id="last_name"
+          name="last_name"
+          value={form.last_name}
+          onChange={handleChange}
+        />
+        <span style={{ color: red }}>{error.last_name}</span>
+        <br />
+        <label htmlFor="mobile_number">mobile Number :</label>
+        <input
+          type="text"
+          id="mobile_number"
+          name="mobile_number"
+          value={form.mobile_number}
+          onChange={handleChange}
+        />
+        <span style={{ color: red }}>{error.mobile_number}</span>
+        <br />
+        <label htmlFor="gender">Gender :</label>
+        <select
+          id="gender"
+          name="gender"
+          value={form.gender}
+          onChange={handleChange}>
+          <option value="">-- select --</option>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+          <option value="other">Other</option>
+        </select>
+        <span style={{ color: red }}>{error.gender}</span>
+        <br />
+        <label htmlFor="address">Address :</label>
+        <input
+          type="text"
+          id="address"
+          name="address"
+          value={form.address}
+          onChange={handleChange}
+        />
+        <span style={{ color: red }}>{error.address}</span>
+        <br />
+        <label htmlFor="Location">Location :</label>
+        <input
+          type="text"
+          id="location"
+          name="location"
+          value={form.location}
+          onChange={handleChange}
+        />
+        <span style={{ color: red }}>{error.location}</span>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
 };
 
 export default StudentForm;
--- src/Machine-coding-round/studentForm/StudentTable.jsx
+++ src/Machine-coding-round/studentForm/StudentTable.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
 
-const StudentTable = () => {
+const StudentTable = React.memo(({ student }) => {
   return (
-    <div>StudentTable</div>
-  )
-}
+    <table border="1">
+      <thead>
+        <tr>
+          <th>No</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Mobile NO</th>
+          <th>Gender</th>
+          <th>Address</th>
+          <th>Location</th>
+        </tr>
+      </thead>
+      <tbody>
+        {student.map((stu, index) => (
+          <tr>
+            <td>{index + 1}</td>
+            <td>{stu.first_name}</td>
+            <td>{stu.last_name}</td>
+            <td>{stu.gender}</td>
+            <td>{stu.mobile_no}</td>
+            <td>{stu.address}</td>
+            <td>{stu.location}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+});
 
-export default StudentTable
+export default StudentTable;
